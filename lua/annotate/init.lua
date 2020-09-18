4ac8caddf58f220e6c56d35d9d809d162d32ba20
Archaengel <god11341258@gmail.com>
2020-09-18 01:26:51 -0700

    feat: Comment single line at cursor position

--- lua/annotate/init.lua
+++ lua/annotate/init.lua
@@ -8,8 +8,7 @@ local function getfmt()
     local commentstring = vim.bo.commentstring
     local commentaryfmt = nil
 
-    if (vim.b["commentary_format"] ~= nil)
-    then
+    if (vim.b["commentary_format"] ~= nil) then
         commentaryfmt = vim.b["commentary_format"]
     else
         commentaryfmt = padcomment(commentstring)
@@ -33,11 +32,47 @@ local function surroundings(fmt)
     return split(fmt, '%%s')
 end
 
-local function testprint()
+local function tocomment(currline)
     local fmt = getfmt()
-    print(vim.inspect(surroundings(fmt)))
+    local splits = surroundings(fmt)
+    local line = splits[1]..currline
+
+    if (#splits > 1) then
+        line = line..splits[2]
+    end
+
+    return line
+end
+
+local function apply(comment, lnum)
+    vim.api.nvim_buf_set_lines(0, lnum - 1,lnum, 1, {comment})
+end
+
+local function attach()
+    vim.api.nvim_set_keymap(
+        'n',
+        'hcc',
+        ':lua require("annotate").annotate()<CR>',
+        {silent = true}
+    )
+end
+
+local function annotate()
+    local lnum = vim.api.nvim_win_get_cursor(0)[1]
+    local currline = vim.api.nvim_buf_get_lines(0, lnum - 1, lnum, 1)[1]
+
+    apply(tocomment(currline), lnum)
+end
+
+local function testprint()
+    local lnum = vim.api.nvim_win_get_cursor(0)[1]
+    local currline = vim.api.nvim_buf_get_lines(0, lnum - 1, lnum, 1)[1]
+    local commented = tocomment(currline)
+    print(commented)
 end
 
 return {
+    annotate = annotate,
+    attach = attach,
     testprint = testprint,
 }
--- plugin/annotate.vim
+++ plugin/annotate.vim
@@ -1,6 +1,7 @@
 function! Annotate()
     lua for k in pairs(package.loaded) do if k:match("^annotate") then package.loaded[k] = nil end end
     lua require("annotate").testprint()
+    lua require("annotate").attach()
 endfun
 
 augroup Annotate
