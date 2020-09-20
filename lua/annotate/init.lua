c3e1214b99a00b83b4b366645adb588f0780d38f
Archaengel <god11341258@gmail.com>
2020-09-20 10:18:54 -0700

    Use Lua builtin  instead of splits

--- lua/annotate/init.lua
+++ lua/annotate/init.lua
@@ -1,51 +1,9 @@
+local nvim_utils = require 'nvim_utils'
+
 local function padcomment(s)
     -- pad literal '%s' with space on both sides
     -- '/*%s*/' -> '/* %s */'
     return s:gsub('(%%s)([^%s])', '%1 %2'):gsub('([^%s])(%%s)', '%1 %2')
-end
-
-local function getfmt()
-    local commentstring = vim.bo.commentstring
-    local commentaryfmt = nil
-
-    if (vim.b["commentary_format"] ~= nil) then
-        commentaryfmt = vim.b["commentary_format"]
-    else
-        commentaryfmt = padcomment(commentstring)
-    end
-
-    return commentaryfmt
-end
-
-local function split(s, sep)
-    local t = {}
-
-    for subs in s:gmatch('([^'..sep..']+)') do
-        -- insert into table t
-        t[#t + 1] = subs
-    end
-
-    return t
-end
-
-local function surroundings(fmt)
-    return split(fmt, '%%s')
-end
-
-local function tocomment(currline)
-    local fmt = getfmt()
-    local splits = surroundings(fmt)
-    local line = splits[1]..currline
-
-    if (#splits > 1) then
-        line = line..splits[2]
-    end
-
-    return line
-end
-
-local function apply(comment, lnum)
-    vim.api.nvim_buf_set_lines(0, lnum - 1,lnum, 1, {comment})
 end
 
 local function attach()
@@ -57,18 +15,28 @@ local function attach()
     )
 end
 
+local function comment_lines(lines)
+    local commentstring = padcomment(vim.bo.commentstring)
+    local commented = {}
+
+    for _, line in ipairs(lines) do
+        commented[#commented + 1] = commentstring:format(line)
+    end
+
+    return commented
+end
+
 local function annotate()
     local lnum = vim.api.nvim_win_get_cursor(0)[1]
     local currline = vim.api.nvim_buf_get_lines(0, lnum - 1, lnum, 1)[1]
-
-    apply(tocomment(currline), lnum)
+    local commented = comment_lines({currline})
+    vim.api.nvim_buf_set_lines(0, lnum - 1,lnum, 1, commented)
 end
 
 local function testprint()
     local lnum = vim.api.nvim_win_get_cursor(0)[1]
     local currline = vim.api.nvim_buf_get_lines(0, lnum - 1, lnum, 1)[1]
-    local commented = tocomment(currline)
-    print(commented)
+    print("Loaded...")
 end
 
 return {
